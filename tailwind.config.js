9b240c8210ba23ec049449e3187db27ccc91cc39
ODINKS <nicholas.okeke87@gmail.com>
2024-05-18 21:33:53 +0100

    fix: adjust the cards width and hover properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'anime-2': {
+          '0%': { transform: 'rotate(5deg)', backgroundColor: 'red' },
+          '50%': { transform: 'rotate(-5deg)', backgroundColor: 'green' },
+          '100%': { transform: 'rotate(0)', backgroundColor: 'yellow' },
+        },
+      },
+      animation: {
+        'anime-2': 'anime-2 1.5s infinite alternate-reverse forwards ease-in-out',
+      },
+    },
   },
   plugins: [],
 }
